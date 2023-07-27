a4d1b0933792779b9c44a9d80533b9d01567be79
Vélimir CHAKHNOVSKI <vchakhno@student.42.fr>
2023-07-27 14:51:46 +0200

    Fixed multiple bugs related to the mouse

--- src/input/mouse_buttons.c
+++ src/input/mouse_buttons.c
@@ -6,7 +6,7 @@
 /*   By: vchakhno <vchakhno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 06:09:51 by vchakhno          #+#    #+#             */
-/*   Updated: 2023/07/26 04:05:16 by vchakhno         ###   ########.fr       */
+/*   Updated: 2023/07/27 14:50:05 by vchakhno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,17 @@
 
 bool	ft_mouse_button_down(t_mouse *mouse, t_button_id button)
 {
-	return (mouse->buttons[button]);
+	return (mouse->buttons[button - 1]);
 }
 
 bool	ft_mouse_button_pressed(t_mouse *mouse, t_button_id button)
 {
-	return (mouse->buttons[button] && !mouse->previous_buttons[button]);
+	return (mouse->buttons[button - 1] && !mouse->previous_buttons[button - 1]);
 }
 
 bool	ft_mouse_button_released(t_mouse *mouse, t_button_id button)
 {
-	return (!mouse->buttons[button] && mouse->previous_buttons[button]);
+	return (!mouse->buttons[button - 1] && mouse->previous_buttons[button - 1]);
 }
 
 bool	ft_mouse_zone_clicked(t_mouse *mouse, t_drect rect, t_button_id button)
@@ -33,6 +33,6 @@ bool	ft_mouse_zone_clicked(t_mouse *mouse, t_drect rect, t_button_id button)
 	return (
 		ft_mouse_button_released(mouse, button)
 		&& ft_rect_contains_point(rect, mouse->pos)
-		&& ft_rect_contains_point(rect, mouse->drag_start_positions[button])
+		&& ft_rect_contains_point(rect, mouse->drag_start_positions[button - 1])
 	);
 }
--- src/math/geometry.c
+++ src/math/geometry.c
@@ -6,7 +6,7 @@
 /*   By: vchakhno <vchakhno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 09:07:48 by vchakhno          #+#    #+#             */
-/*   Updated: 2023/07/26 04:05:38 by vchakhno         ###   ########.fr       */
+/*   Updated: 2023/07/27 14:39:26 by vchakhno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ bool	ft_circle_contains(t_circle circle, t_point point)
 bool	ft_rect_contains_point(t_drect rect, t_point point)
 {
 	return (
-		(rect.x <= point.x) && (point.x <= rect.x + rect.w)
-		&& (rect.y <= point.y) && (point.y <= rect.y + rect.h)
+		(rect.x <= point.x) && (point.x < rect.x + rect.w)
+		&& (rect.y <= point.y) && (point.y < rect.y + rect.h)
 	);
 }
